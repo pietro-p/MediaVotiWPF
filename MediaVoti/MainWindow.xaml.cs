623fb5265a75b9a729cc1918a8b3f77c7f62b195
_ Trentini _ <pietrotrentini5@gmail.com>
2021-03-20 09:59:13 +0100

    modifica minimo e massimo

--- MediaVoti/MainWindow.xaml.cs
+++ MediaVoti/MainWindow.xaml.cs
@@ -27,8 +27,9 @@ namespace MediaVoti
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-             const int COMPITI = 4;
-            if(txtCompito1.Text==""|| txtCompito2.Text=="" || txtCompito3.Text == "" || txtCompito4.Text == "")
+
+            const int COMPITI = 4;
+            if (txtCompito1.Text == "" || txtCompito2.Text == "" || txtCompito3.Text == "" || txtCompito4.Text == "")
             {
                 MessageBox.Show("Non sono stati compilati tutti i campi", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
@@ -38,11 +39,15 @@ namespace MediaVoti
                 double compito2 = double.Parse(txtCompito2.Text);
                 double compito3 = double.Parse(txtCompito3.Text);
                 double compito4 = double.Parse(txtCompito4.Text);
-                if(compito1 >=2 && compito1 <=10 && compito2 >= 2 && compito2 <= 10 && compito3 >= 2 && compito3 <= 10 && compito4 >= 2 && compito4 <= 10)
+                if (compito1 >= 2 && compito1 <= 10 && compito2 >= 2 && compito2 <= 10 && compito3 >= 2 && compito3 <= 10 && compito4 >= 2 && compito4 <= 10)
                 {
                     double somma = compito1 + compito2 + compito3 + compito4;
                     double media = somma / COMPITI;
                     lblMedia.Content = media;
+                    double min = Math.Min(compito1,(Math.Min(compito2,(Math.Min(compito3, compito4)))));
+                    double max = Math.Max(compito1, (Math.Max(compito2, (Math.Max(compito3, compito4)))));
+                    lblMin.Content = min;
+                    lblMax.Content = max;
                     if (media >= 6)
                         lblRisposta.Content = "Studente PROMOSSO";
                     else
@@ -51,7 +56,7 @@ namespace MediaVoti
                 else
                 {
                     MessageBox.Show("Inserire un voto da 2 a 10", "Attenzione", MessageBoxButton.OK, MessageBoxImage.Warning);
-                }    
+                }
             }
             catch (Exception ex)
             {
